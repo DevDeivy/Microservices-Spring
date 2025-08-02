6725bdc372f726e50195f9c620696b9048928ad2
DevDeivy <deivylujan@hotmail.com>
2025-08-02 17:58:27 -0500

    :construction: add some microservices, work in progress

---
+++ microservices/costumer-microservices/src/main/java/com/DevDeivy/microservices/costumer_microservices/CostumerMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.DevDeivy.microservices.costumer_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostumerMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostumerMicroservicesApplication.class, args);
+	}
+
+}
---
+++ microservices/costumer-microservices/src/test/java/com/DevDeivy/microservices/costumer_microservices/CostumerMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevDeivy.microservices.costumer_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostumerMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
